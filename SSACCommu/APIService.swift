c094898edbcc332ef6a406cbaac0b9a23d804fc9
yoogail105 <yoogail105@gmail.com>
2022-01-05 03:30:22 +0900

    sign up url session

--- SSACCommu/APIService.swift
+++ SSACCommu/APIService.swift
@@ -21,7 +21,8 @@ class APIService {
     static func signUp(userName: String, email: String, password: String, completion: @escaping (User?, APIError?) -> Void ) {
         var request = URLRequest(url: Endpoint.signUp.url)
         request.httpMethod = Method.POST.rawValue
-        request.httpBody = "username:\(userName)&email=\(email)&password=\(password)".data(using: .utf8, allowLossyConversion: false)
+        request.httpBody = "username=\(userName)&email=\(email)&password=\(password)".data(using: .utf8, allowLossyConversion: false)
+//        request.httpBody = "username=hoi&email=ss@ss.com&password=1234".data(using: .utf8, allowLossyConversion: false)
 
         URLSession.request(endpoint: request, completion: completion)
     }
